a9964fa299a148a7d17e03f25dced42dd0f65ffc
Vignesh B <vigneshbv02@gmail.com>
2018-03-02 18:35:11 +0530

    s

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/',function(req,res){
 
 app.get('/node',function(req,res){
     console.log(req.query.data);
-    res.send("Data Received from "+req.query.data);
+    res.send("Data Received from "+req.query.data+" .");
 });
 
 var server=http.listen(process.env.PORT || 5000,function(){
