9b9483e0daadb9f3e294cd8d25010a947d472824
Vignesh B <vigneshbv02@gmail.com>
2018-03-02 18:28:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express=require("express");
+var app=express();
+var bodyparser=require("body-parser");
+
+var http=require('http').Server(app);
+var io=require('socket.io')(http);
+
+var data=0;
+
+app.use(bodyparser.json({limit: '50mb'}));
+app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
+
+app.get('/',function(req,res){
+    res.send("Welcome to Node Optimizer");
+});
+
+app.get('/node',function(req,res){
+    console.log(req.query.data);
+    res.send("Data Received from "+req.query.data);
+});
+
+var server=http.listen(process.env.PORT || 5000,function(){
+    console.log("server running in port "+(process.env.PORT || 5000));
+});
+
