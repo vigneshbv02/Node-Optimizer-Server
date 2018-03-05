a1d98b24e853b6ec36ac001ad5fb903a6200541c
Vignesh B <vigneshbv02@gmail.com>
2018-03-05 20:03:47 +0530

    s

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var http=require('http').Server(app);
 var io=require('socket.io')(http);
 
 var data=0;
+var data1=0,data2=0,data3=0,data4=0,data5=0,;
+var node_no="";
+var ssid1="",ssid2="",ssid3="",ssid4="",ssid5="",;
 
 app.use(bodyparser.json({limit: '50mb'}));
 app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
@@ -17,6 +20,30 @@ app.get('/',function(req,res){
 app.get('/node',function(req,res){
     console.log(req.query.data);
     console.log(req.query.via);
+    console.log(req.query.node_no);
+    switch (node_no)
+    {
+        case "node1":
+            data1=data;
+            ssid1=via;
+            break;
+        case "node2":
+            data2=data;
+            ssid2=via;
+            break;
+        case "node3":
+            data3=data;
+            ssid3=via;
+            break;
+        case "node4":
+            data4=data;
+            ssid4=via;
+            break;
+        case "node5":
+            data5=data;
+            ssid5=via;
+            break;
+    }
     res.send("Data Received from "+req.query.data+" via"+req.query.via);
 });
 
