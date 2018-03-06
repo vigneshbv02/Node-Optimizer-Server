4ced04817df57b078c49a43f9b89fcb6f4252b72
Vignesh B <vigneshbv02@gmail.com>
2018-03-07 01:43:48 +0530

    s

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
 io.on("connection",function(socket)
 {
     console.log("A user connected:" + socket.id);
-    socket.join("room01-node_optimizer");
+    socket.join("room01-nodeoptimizer");
     socket.emit("notify",{'message':"Welcome to Node_Optimizer"});
 });
 
@@ -34,27 +34,27 @@ app.get('/node',function(req,res){
         case "node1":
             data1=data;
             ssid1=via;
-            io.to("room01-node_optimizer").emit('node1',{'data':data1,'ssid':ssid1});
+            io.to("room01-nodeoptimizer").emit('node1',{'data':data1,'ssid':ssid1});
             break;
         case "node2":
             data2=data;
             ssid2=via;
-            io.to("room01-node_optimizer").emit('node2',{'data':data2,'ssid':ssid2});
+            io.to("room01-nodeoptimizer").emit('node2',{'data':data2,'ssid':ssid2});
             break;
         case "node3":
             data3=data;
             ssid3=via;
-            io.to("room01-node_optimizer").emit('node3',{'data':data3,'ssid':ssid3});
+            io.to("room01-nodeoptimizer").emit('node3',{'data':data3,'ssid':ssid3});
             break;
         case "node4":
             data4=data;
             ssid4=via;
-            io.to("room01-node_optimizer").emit('node4',{'data':data4,'ssid':ssid4});
+            io.to("room01-nodeoptimizer").emit('node4',{'data':data4,'ssid':ssid4});
             break;
         case "node5":
             data5=data;
             ssid5=via;
-            io.to("room01-node_optimizer").emit('node5',{'data':data5,'ssid':ssid5});
+            io.to("room01-nodeoptimizer").emit('node5',{'data':data5,'ssid':ssid5});
             break;
     }
     res.send("Data Received from "+req.query.data+" via"+req.query.via);
