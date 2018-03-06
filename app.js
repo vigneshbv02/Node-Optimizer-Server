4ce69485d33f2869f1c11bcabc9866f6889aed3b
Vignesh B <vigneshbv02@gmail.com>
2018-03-07 02:10:55 +0530

    bhj

--- app.js
+++ app.js
@@ -32,12 +32,27 @@ app.get('/node',function(req,res){
     if(req.query.nodeno==='node1')
     {
         console.log("Sending data to socket for node 1");
-        io.to("room01-nodeoptimizer").emit('node1',{'data':"Grocery level is LOW",'ssid':"nodeoptimizer"});
+        io.to("room01-nodeoptimizer").emit('node1',{'data':data,'ssid':via});
     }
     else if(req.query.nodeno==='node2')
     {
         console.log("Sending data to socket for node 1");
-        io.to("room01-nodeoptimizer").emit('node2',{'data':"Grocery level is LOW",'ssid':"nodeoptimizer"});
+        io.to("room01-nodeoptimizer").emit('node2',{'data':data,'ssid':via});
+    }
+    else if(req.query.nodeno==='node3')
+    {
+        console.log("Sending data to socket for node 1");
+        io.to("room01-nodeoptimizer").emit('node3',{'data':data,'ssid':via});
+    }
+    else if(req.query.nodeno==='node4')
+    {
+        console.log("Sending data to socket for node 1");
+        io.to("room01-nodeoptimizer").emit('node2',{'data':data,'ssid':via});
+    }
+    else if(req.query.nodeno==='node5')
+    {
+        console.log("Sending data to socket for node 1");
+        io.to("room01-nodeoptimizer").emit('node2',{'data':data,'ssid':via});
     }
     res.send("Data Received from "+req.query.data+" via"+req.query.via);
 });
