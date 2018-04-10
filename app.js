67f094bbd2284a26d5c84ae3437b6b6b39e7fc8c
Vignesh. B <vigneshbv02@gmail.com>
2018-04-11 00:33:43 +0530

    sa

--- app.js
+++ app.js
@@ -43,27 +43,29 @@ app.get('/node',function(req,res){
         io.to("room01-nodeoptimizer").emit('node1',{'data':req.query.data,'ssid':req.query.via});
         var vignesh_wifi_len = req.query.vignesh;
         var dude_wifi_len = req.query.dude;
-        vignesh_wifi_len /=100;
-        dude_wifi_len /=100;
-        console.log("Base station 1"+vignesh_wifi_len);
-        console.log("Base station 2"+dude_wifi_len);
-        var d1 = (((thre[0][0] - vignesh_wifi_len) * (thre[0][0] - vignesh_wifi_len)) +
-            ((thre[1][0] - vignesh_wifi_len) * (thre[1][0] - vignesh_wifi_len)) +
-            ((thre[2][0] - vignesh_wifi_len) * (thre[2][0] - vignesh_wifi_len)) +
-            ((thre[3][0] - vignesh_wifi_len) * (thre[2][0] - vignesh_wifi_len))).toFixed(2);
+        console.log("Base station 1 : "+vignesh_wifi_len);
+        console.log("Base station 2 : "+dude_wifi_len);
+        vignesh_wifi_len1 =vignesh_wifi_len/100;
+        dude_wifi_len1 =dude_wifi_len/100;
+        console.log("Base station 1"+vignesh_wifi_len1);
+        console.log("Base station 2"+dude_wifi_len1);
+        var d1 = (((thre[0][0] - vignesh_wifi_len1) * (thre[0][0] - vignesh_wifi_len1)) +
+            ((thre[1][0] - vignesh_wifi_len1) * (thre[1][0] - vignesh_wifi_len1)) +
+            ((thre[2][0] - vignesh_wifi_len1) * (thre[2][0] - vignesh_wifi_len1)) +
+            ((thre[3][0] - vignesh_wifi_len1) * (thre[2][0] - vignesh_wifi_len1))).toFixed(2);
         console.log(d1);
 
-        var d2 = (((thre[0][1] - dude_wifi_len) * (thre[0][1] - dude_wifi_len)) +
-            ((thre[1][1] - dude_wifi_len) * (thre[1][1] - dude_wifi_len)) +
-            ((thre[2][1] - dude_wifi_len) * (thre[2][1] - dude_wifi_len)) +
-            ((thre[3][1] - dude_wifi_len) * (thre[2][1] - dude_wifi_len))).toFixed(2);
+        var d2 = (((thre[0][1] - dude_wifi_len1) * (thre[0][1] - dude_wifi_len1)) +
+            ((thre[1][1] - dude_wifi_len1) * (thre[1][1] - dude_wifi_len1)) +
+            ((thre[2][1] - dude_wifi_len1) * (thre[2][1] - dude_wifi_len1)) +
+            ((thre[3][1] - dude_wifi_len1) * (thre[2][1] - dude_wifi_len1))).toFixed(2);
         console.log(d2);
         var result='';
-        if(d1<d2)
+        if(d1<d2 && d1 !== 0)
         {
             result='vignesh';
         }
-        else
+        else if(d1>=d2&& d2 !== 0)
         {
             result='dude';
         }
